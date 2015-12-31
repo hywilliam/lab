43159946d6cef52fe75234d410ed37eac83c4092
hyWilliam <459288729@qq.com>
2016-01-01 00:02:40 +0800

    atom commit test

--- Angular/play.js
+++ Angular/play.js
@@ -1,7 +1,12 @@
 //emmet 插件导致补全的时候一回车就换行真烦，试一下js是不是ok
 
-var app = angular.module('myApp', []);
+var app = angular.module('myTest', []);
 
-app.controller('test', function($scope){
-    //js 不会，只是emmet插件的问题
+app.controller('test', function($scope) {
+	//js 不会，只是emmet插件的问题
+	$scope.message = 'rabbit';
+	$scope.closeShow = function() {
+		console.log('here');
+		$scope.value = !$scope.value;
+	};
 });
